5a3484184f43e71b7df04048f5f6d3fb8b0d20a3
Tanvir Sajed <tsajed@ualberta.ca>
2016-10-02 04:29:13 -0600

    Add functions to new problem for CheckUniqueChar

--- c++/check_unique_char.cpp
+++ c++/check_unique_char.cpp
@@ -1,2 +1,25 @@
+// Solution to problem from Crack Coding interviews. Implement an 
+// algorithm to determine if a string has all unique characters
 #include "check_unique_char.h"
 
+CheckUniqueChar::CheckUniqueChar() {
+
+}
+
+CheckUniqueChar::~CheckUniqueChar() {
+
+}
+
+// Takes O(n) running time
+bool CheckUniqueChar::solver_boolean(std::string test_string) {
+
+}
+
+// Takes O(nlogn) running time
+bool CheckUniqueChar::solver_sorted(std::string test_string) {
+
+} 
+
+bool CheckUniqueChar:test_case() {
+
+}
